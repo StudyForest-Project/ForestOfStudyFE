1081ca5770ec2a9d2a229f5455bb1b1c2e730bda
박종장 <pjjcmr@gmail.com>
2026-01-29 11:03:47 +0900

    최근조회한스터디 백앤드/박종장

--- src/components/Study/sections/Recent.page.jsx
+++ src/components/Study/sections/Recent.page.jsx
@@ -1,5 +1,8 @@
 import { Link } from 'react-router-dom';
 import styles from './StudyPage.module.css';
+import { useRecentStudies } from ''
+
+
 
 export const RecentPage = () => {
   return (
@@ -9,3 +12,29 @@ export const RecentPage = () => {
     </section>
   );
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 컴포넌트 규칙 지키기
+// 만들때 폴더 생성 해서 만들기
+// set 은 온클릭까지 
+// get으로 카드 컴포넌트를 어떻게 쓸건지 파악하는 행위 => 로컬스토리지 겟으로 꺼내오는것
+// 카드는 규량님 카드를 
+
+
+
---
+++ src/hooks/useRecentStudies.js
@@ -0,0 +1,4 @@
+import { useState, useEffect } from 'react';
+
+const STORAGE_KEY = 'recentStudies';
+const MAX_RECENT = 10;
