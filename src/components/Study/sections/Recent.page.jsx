eccad447470ee32faa0eeb8c637832fccb486aaa
박종장 <pjjcmr@gmail.com>
2026-01-30 15:36:29 +0900

    로컬스토리지 전반적 업데이트/박종장

--- src/components/Study/sections/Recent.page.jsx
+++ src/components/Study/sections/Recent.page.jsx
@@ -1,40 +1,61 @@
 import { Link } from 'react-router-dom';
 import styles from './StudyPage.module.css';
 import { useRecentStudies } from '../../../hooks/useRecentStudies';
+import { StudyCard } from './StudyCard'; //! 추가
+// import { recentStudySeedData } from '../seed/seedStudyData'; // 향후 수정하여 반영
+// import  sections from './Recent.page..module.css'; //향후 수정하여 반영
 
 export const RecentPage = () => {
-  const { recentStudies } = useRecentStudies();
+  const { recentStudies, setRecentStudiesData } = useRecentStudies();
 
   const hasRecent = recentStudies.length > 0;
   const visibleStudies = recentStudies.slice(0, 3); // 화면에는 최대 3개만
+
+  const handleSetSeedData = () => {
+    setRecentStudiesData(recentStudySeedData);
+    alert('시드 데이터가 저장되었습니다.'); // 경고알림 텍스트 뜸
+  };
+
+  const handleClearData = () => {
+    setRecentStudiesData([]);
+    alert('최근 조회 데이터가 초기화되었습니다.'); // 경고알림 텍스트 뜸
+  };
+
+  const handleShowLocalStorage = () => {
+    const stored = localStorage.getItem('recentStudies');
+    console.log('[recentStudies raw]', stored);
+    console.log('[recentStudies parsed]', JSON.parse(stored || '[]'));
+  };
 
   return (
     <section className={styles.container}>
       <h1>최근 조회한 스터디</h1>
 
-      {!hasRecent && <p>아직 조회한 스터디가 없어요</p>}
-
+      {!hasRecent && <p>아직 조회한 스터디가 없습니다.</p>}
       {hasRecent && (
         <div className={styles.recentList}>
           {visibleStudies.map((item) => (
             <StudyCard
               key={item.studyId}
               studyId={item.studyId}
-              // 필요하면 viewedAt도 넘겨서 "언제 봤는지" 표시 가능하게 해놓음
+              // 언제 조회했는지 확인하는 옵션 ==> viewedAt
               viewedAt={item.viewedAt}
             />
           ))}
         </div>
       )}
 
+      {/* 온클릭 부분 */}
+      {/* {process.env.NODE_ENV === 'development' && (
+        <div className={styles.devButtonGroup}>
+          <button onClick={handleSetSeedData}>시드 데이터 SET</button>
+          <button onClick={handleClearData}>초기화</button>
+          <button onClick={handleShowLocalStorage}>콘솔 출력</button>
+        </div>
+      )} */}
+
       {/* 테스트용 링크 */}
       <Link to="/study/1">상세페이지 예시로 이동</Link>
     </section>
   );
 };
-
-// 컴포넌트 규칙 지키기
-// 만들때 폴더 생성 해서 만들기
-// set 은 온클릭까지
-// get으로 카드 컴포넌트를 어떻게 쓸건지 파악하는 행위 => 로컬스토리지 겟으로 꺼내오는것
-// 카드는 규량님 카드를
