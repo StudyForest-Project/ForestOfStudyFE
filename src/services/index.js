b01f13412435869973b10cf7d3df375f16346fc3
BackNo23 <kuzhi.no.23@gmail.com>
2026-02-04 10:34:59 +0900

    오타수정

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,5 @@
 export { api, publicApi } from './api';
 export { default as authService } from './authService';
-// export { default as focusService } from './focusService';
 export * from './studyService';
-export * from './HabitService';
+export * from './habitService';
 export * from './studyAuthService';
